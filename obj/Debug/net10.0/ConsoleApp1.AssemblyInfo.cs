1044335d2d7cfd3ed5a0c9b4284399dd206d5dd4
Mohammad Ghazal <mohammedghazaleng@gmail.com>
2026-02-25 02:01:44 +0300

    Name Issue

--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7336ff83e1c5d1b82d3cd9ef7d01562a5ac8ac3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3269645cf9151a69dd39c6b7b06a64d93d90794b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
